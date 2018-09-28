8e147dc70461495f95affa478d718a02a5bb8269
federico.olivieri <federico.olivieri@ticketmaster.co.uk>
2018-09-28 14:17:40 +0100

    add ping telemetry code

---
+++ telemetry-ping.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+import os, re, datetime, time, threading
+from influxdb import InfluxDBClient
+from credPass import credPass
+
+def facebook():
+    while True:
+        ping = os.popen("ping -c 1 facebook.com")
+        rtt = rttTime.search(ping.read())
+        if rtt:
+            value = float(rtt.group(2))
+        else:
+            value = 0
+        jsonBody = [
+         {
+             "measurement": "ping_rtt",
+             "tags": {
+                 "host": "facebook.com",
+             },
+             "time": str(datetime.datetime.today()),
+             "fields": {
+                 "value": value
+             }
+         }
+        ]
+        client.write_points(jsonBody)
+        time.sleep(3)
+
+def google():
+    while True:
+        ping = os.popen("ping -c 1 google.com")
+        rtt = rttTime.search(ping.read())
+        if rtt:
+            value = float(rtt.group(2))
+        else:
+            value = 0
+        jsonBody = [
+         {
+             "measurement": "ping_rtt",
+             "tags": {
+                 "host": "google.com",
+             },
+             "time": str(datetime.datetime.today()),
+             "fields": {
+                 "value": value
+             }
+         }
+        ]
+        client.write_points(jsonBody)
+        time.sleep(3)
+
+if __name__ == '__main__':
+    rttTime = re.compile(r'(time=)(\d+\.\d+)')
+    influx = credPass()
+    client = InfluxDBClient(host='10.255.255.254', port=8086, username=influx.load('db','username'), password=influx.load('db','password'), database='ping')
+    fbDef = threading.Thread(name='facebook', target=facebook)
+    glDef = threading.Thread(name='google', target=google)
+    fbDef.start()
+    glDef.start()
