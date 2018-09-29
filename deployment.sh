8c42a861f011d9e29143384f7f21fe15d97515be
federico.olivieri <federico.olivieri@ticketmaster.co.uk>
2018-09-29 11:30:26 +0100

    delete rm docker

--- deployment.sh
+++ deployment.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # INFLUXDB
-docker rm -f db
 docker run --name db -d \
       -e INFLUXDB_DB=network_telemetry \
       -e INFLUXDB_ADMIN_USER=root -e INFLUXDB_ADMIN_PASSWORD=supersecretpassword \
@@ -10,15 +9,12 @@ docker run --name db -d \
       influxdb
 
 # RUN GRAFANA AND LINK TO DB
-docker rm -f grafana
 docker run -d --name=grafana --link db -p 3000:3000 grafana/grafana
 
 # BUILD PING PROBE CONTAINER AND LINK TO DB
 docker build ping-probes/ -t network-telemetry-ping:latest
-docker rm -f ping
 docker run -d --name=ping --link db --restart=always network-telemetry-ping
 
 # BUILD PING PROBE CONTAINER AND LINK TO DB
 docker build tcp-probes/ -t network-telemetry-tcp:latest
-docker rm -f tcp
 docker run -d --name=tcp --link db --restart=always network-telemetry-tcp
