e2b518708377c53d20503c999d075236e886fde0
federico.olivieri <federico.olivieri@ticketmaster.co.uk>
2018-10-02 15:04:47 +0100

    update

--- ping-probes/network-telemetry-ping.py
+++ ping-probes/network-telemetry-ping.py
@@ -72,13 +72,34 @@ def yahoo():
         client.write_points(jsonBody)
         time.sleep(3)
 
+def ansa():
+    while True:
+        ping = os.popen("ping -c 1 www.ansa.it")
+        rtt = rttTime.search(ping.read())
+        if rtt:
+            value = float(rtt.group(2))
+        else:
+            value = float(0.000)
+        jsonBody = [
+         {
+             "measurement": "ping_rtt",
+             "tags": {
+                 "target": "ansa.it",
+             },
+             "time": str(datetime.datetime.today()),
+             "fields": {
+                 "value": value
+             }
+         }
+        ]
+        client.write_points(jsonBody)
+        time.sleep(3)
+
 if __name__ == '__main__':
     rttTime = re.compile(r'(time=)(\d+\.\d+)')
     influx = credPass()
     client = InfluxDBClient(host='db', port=8086, username=influx.load('influxdb','username'), password=influx.load('influxdb','password'), database='network_telemetry')
-    fbDef = threading.Thread(name='facebook', target=facebook)
-    glDef = threading.Thread(name='google', target=google)
-    yhDef = threading.Thread(name='yahoo', target=yahoo)
-    fbDef.start()
-    glDef.start()
-    yhDef.start()
+    threading.Thread(name='facebook', target=facebook).start()
+    threading.Thread(name='google', target=google).start()
+    threading.Thread(name='yahoo', target=yahoo).start()
+    threading.Thread(name='ansa', target=ansa).start()
