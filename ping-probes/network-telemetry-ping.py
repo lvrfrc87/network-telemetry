39d21bff0250044529579ad192e797cfcb410d4e
federico.olivieri <federico.olivieri@ticketmaster.co.uk>
2018-10-02 12:51:15 +0100

    add yahoo telemtry

--- ping-probes/network-telemetry-ping.py
+++ ping-probes/network-telemetry-ping.py
@@ -49,11 +49,36 @@ def google():
         client.write_points(jsonBody)
         time.sleep(3)
 
+def yahoo():
+    while True:
+        ping = os.popen("ping -c 1 www.yahoo.com")
+        rtt = rttTime.search(ping.read())
+        if rtt:
+            value = float(rtt.group(2))
+        else:
+            value = float(0.000)
+        jsonBody = [
+         {
+             "measurement": "ping_rtt",
+             "tags": {
+                 "target": "yahoo.com",
+             },
+             "time": str(datetime.datetime.today()),
+             "fields": {
+                 "value": value
+             }
+         }
+        ]
+        client.write_points(jsonBody)
+        time.sleep(3)
+
 if __name__ == '__main__':
     rttTime = re.compile(r'(time=)(\d+\.\d+)')
     influx = credPass()
     client = InfluxDBClient(host='db', port=8086, username=influx.load('influxdb','username'), password=influx.load('influxdb','password'), database='network_telemetry')
     fbDef = threading.Thread(name='facebook', target=facebook)
     glDef = threading.Thread(name='google', target=google)
+    yhDef = threading.Thread(name='yahoo', target=yahoo)
     fbDef.start()
     glDef.start()
+    yhDef.start()
--- tcp-probes/network-telemetry-tcp.py
+++ tcp-probes/network-telemetry-tcp.py
@@ -97,11 +97,60 @@ def google():
                 client.write_points(jsonBodyRtt)
         time.sleep(2)
 
+def yahoo():
+    while True:
+        nping = os.popen('nping --tcp -c 1 --dest-port 443 www.yahoo.com')
+        npingRead = nping.read()
+        for i in npingRead.splitlines():
+            if 'SENT' in i:
+                rttTime = reTime.search(i)
+                value = float(rttTime.group())
+                if value:
+                    pass
+                else:
+                    value = float(0.000)
+                jsonBodyRtt = [
+                 {
+                     "measurement": "rtt_tcp_sent",
+                     "tags": {
+                         "target": "yahoo.com",
+                     },
+                     "time": str(datetime.datetime.today()),
+                     "fields": {
+                         "value": value
+                     }
+                 }
+                ]
+                client.write_points(jsonBodyRtt)
+            elif 'RCVD' in i:
+                rttTime = reTime.search(i)
+                value = float(rttTime.group())
+                if value:
+                    pass
+                else:
+                    value = float(0.000)
+                jsonBodyRtt = [
+                 {
+                     "measurement": "rtt_tcp_rcvd",
+                     "tags": {
+                         "target": "yahoo.com",
+                     },
+                     "time": str(datetime.datetime.today()),
+                     "fields": {
+                         "value": value
+                     }
+                 }
+                ]
+                client.write_points(jsonBodyRtt)
+        time.sleep(2)
+
 if __name__ == '__main__':
     reTime = re.compile(r'(\d+\.\d+)')
     influx = credPass()
     client = InfluxDBClient(host='db', port=8086, username=influx.load('influxdb','username'), password=influx.load('influxdb','password'), database='network_telemetry')
     fbDef = threading.Thread(name='facebook', target=facebook)
     glDef = threading.Thread(name='google', target=google)
+    yhDef = threading.Thread(name='yahoo', target=yahoo)
     fbDef.start()
     glDef.start()
+    yhDef.start()
