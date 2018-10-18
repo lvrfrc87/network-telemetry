52d925d1f926b68a4ff96f5efc0c26dbde5044e0
federico.olivieri <federico.olivieri@ticketmaster.co.uk>
2018-10-18 22:30:47 +0100

    Update ping code. Ver 2.0

--- ping-probes/network_telemetry_ping.py
+++ ping-probes/network_telemetry_ping.py
@@ -1,67 +1,39 @@
-#!/usr/bin/env python3
-''' Code for ping probe. The command is executed every
-    3 seconds and the RTT is extraxcted via regex.
-    The value is stored in one or more InfluxDb instance'''
-import os
-import re
-import datetime
+import yaml
 import time
 import threading
-import yaml
+from ping import Ping
+from influxdb import InfluxDBClient
 from credPass import credPass
 from urllib3.exceptions import NewConnectionError
 from urllib3.exceptions import MaxRetryError
 from requests.exceptions import ConnectionError as ApiCallError
-from influxdb import InfluxDBClient
 
 def thread_ping():
-    ''' multithreading ping probes '''
+    """ threading for ping probes """
     ping_threads = []
     for region, target_list in dic_targets.items():
         for target in target_list:
-            thread_targets = threading.Thread(target=ping_probe, args=(region, target))
+            thread_targets = threading.Thread(target=influxdb_call, args=(target, region))
             thread_targets.start()
             ping_threads.append(thread_targets)
 
-def ping_probe(region, target):
-    ''' ping probe execution and regex rtt '''
-    ping = os.popen("ping -c 1 {}".format(target))
-    rtt = rtt_time.search(ping.read())
-    if rtt:
-        value = float(rtt.group(2))
-    else:
-        value = float(0.000)
-    json_body_ping = [
-        {
-            "measurement": "ping_rtt",
-            "tags": {
-                "target": target,
-                "region": region},
-            "time": str(datetime.datetime.today()),
-            "fields": {
-                "value": value}
-        }
-    ]
+def influxdb_call(target, region):
     for client in db_list:
         try:
-            connect = InfluxDBClient(
+            connect  = InfluxDBClient(
                 host=client,
                 port=8086,
-                username=influx.load(client, 'username'),
-                password=influx.load(client, 'password'),
+                username=credPass().load(client, 'username'),
+                password=credPass().load(client, 'password'),
                 database='network_telemetry')
-            connect.write_points(json_body_ping)
+            connect.write_points(Ping(target, region).run_ping())
         except (NewConnectionError, MaxRetryError, ApiCallError) as error:
             print(error)
+
 if __name__ == '__main__':
     dic_targets = yaml.load(open('var/targets.yaml', 'rb'))
-    rtt_time = re.compile(r'(time=)(\d+\.\d+)')
-    influx = credPass()
-    # Add DB hostname/IP to db_list in case you want send result to more than one DB.
-    # Remember to update .credential.json with DBs login.
     db_list = [
-        'db'
+        'db',
         ]
     while True:
         thread_ping()
-        time.sleep(3)
---
+++ ping-probes/ping.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""python class to extract ping values from ping UNIX command
+and returned in json format ready to be written on InfluxDVself.
+Below an utput example:
+
+PING www.google.com (216.58.216.4): 56 data bytes
+64 bytes from 216.58.216.4: icmp_seq=0 ttl=42 time=130.795 ms
+
+--- www.google.com ping statistics ---
+1 packets transmitted, 1 packets received, 0.0% packet loss
+round-trip min/avg/max/stddev = 130.795/130.795/130.795/0.000 ms
+
+Values extracted in float datat type are:
+1 - Packet transmitted
+2 - Packet received
+3 - Packet loss
+4 - Min response time
+5 - Average response time
+6 - Max response time
+7 - Delta between Min and Max response time
+"""
+import os
+import datetime
+import re
+
+class Ping():
+    """__init__ and run_ping methods"""
+    def __init__(self, target, region):
+        self.target = target
+        self.region = region
+
+    def run_ping(self):
+        """ run ping command and extract values"""
+        ping_cmd = os.popen("ping -c 1 -t 1 {}".format(self.target))
+        regex = re.compile(r'\d+\.?')
+        values = ping_cmd.read().split()
+        json_body = [
+            {
+                "measurement": "ping_rtt",
+                "tags": {
+                    "host": self.target,
+                    "region": self.region},
+                "time": str(datetime.datetime.today()),
+                "fields": {
+                    "transmitted": float(values[19]),
+                    "received": float(values[22]),
+                    "loss": float(regex.search(values[25]).group()),
+                    "min": float(values[31].split("/")[0]),
+                    "avg": float(values[31].split("/")[1]),
+                    "max": float(values[31].split("/")[2]),
+                    "stddev": float(values[31].split("/")[3])}
+            }
+        ]
+        return json_body
